e93f74e56999d6ceb699a365fc0a3f25094614ec
engineervinay <vinay1645.vp@gmail.com>
2022-02-08 01:17:53 +0530

    escape

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
-
+import { useState } from "react";
 function App() {
+var [userInput,setuserInput]=usestate("search text");
+var url;
 
-var[searchResult, setsearchResult]=useState("Search Text");
 function searchChange(event){
-  var userInput=event.target.value;
-
+  var searchkey=event.target.value;
+  setuserInput(searchkey);
+console.log("hello");
 }
 
-
+function searchOperation(event){
+  var url="http://hn.algolia.com/api/v1/search_by_date?query="+userInput;
+  console.log(url);
+}
   return (
     <div className="App">
       <input onChange={searchChange} type="text" className="search-bar" placeholder="search"/>
-      <button >Search</button>
+      <button onClick={searchOperation}>Search</button>
 
 
     </div>
