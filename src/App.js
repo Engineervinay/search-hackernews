dc21a2ffb41cbdd921db2e428f977264a93dc4d6
engineervinay <vinay1645.vp@gmail.com>
2022-02-08 01:03:58 +0530

    adding search feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 
 function App() {
+
+var[searchResult, setsearchResult]=useState("Search Text");
+function searchChange(event){
+  var userInput=event.target.value;
+
+}
+
+
   return (
     <div className="App">
-      <input type="text" className="search-bar" placeholder="search"/>
-      <button>Search</button>
-
-
+      <input onChange={searchChange} type="text" className="search-bar" placeholder="search"/>
+      <button >Search</button>
 
 
     </div>
